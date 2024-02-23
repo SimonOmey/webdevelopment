752865ff96d729cbe5dce8eed8bc52ae6866e04f
SimonOmey <simon.omey@student.vives.be>
2024-02-23 13:27:36 +0100

    substring gefixd

--- Sem 2 Labo 2/Opdracht Substring/scripts/code.js
+++ Sem 2 Labo 2/Opdracht Substring/scripts/code.js
@@ -1,14 +1,16 @@
 const setup = () => {
-    let substring = document.getElementById("substring")
-
-    substring.addEventListener("click", splits)
+    let btnSubstring = document.getElementById("substring")
+    btnSubstring.addEventListener("click", splits)
 }
-window.addEventListener("load", setup);
+
 
 const splits = () =>{
     let txtInput = document.getElementById("txtInput")
-    let startNmr = parseInt(document.getElementById("startNummer").value)
-    let eindNmr = parseInt(document.getElementById("eindNummer").value)
     let txtOutput = document.getElementById("txtOutput")
-    txtOutput.innerHTML = txtInput.substring(startNmr,eindNmr)
+    startNmr = parseInt(document.getElementById("startNummer").value)
+    eindNmr = parseInt(document.getElementById("eindNummer").value)
+
+    let substring = txtInput.value.substring(startNmr,eindNmr);
+    txtOutput.innerHTML = substring;
 }
+window.addEventListener("load", setup);

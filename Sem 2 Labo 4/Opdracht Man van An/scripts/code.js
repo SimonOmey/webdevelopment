9089c1f5e67ffa8291536902b475c1b4763cb0ef
SimonOmey <simon.omey@student.vives.be>
2024-03-07 17:40:43 +0100

    Opdracht Man van An

--- Sem 2 Labo 4/Opdracht Man van An/scripts/code.js
+++ Sem 2 Labo 4/Opdracht Man van An/scripts/code.js
@@ -1,5 +1,21 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+	let button = document.getElementById("button")
+    button.addEventListener('click',berekenAn)
+}
+
+const berekenAn = () =>{
+    let input = document.getElementById("input").value
+    input = input.toLowerCase();
+    let aantal = document.getElementById("aantal")
+    let index = 0
+    let aantalKeerAn = 0
+
+    while(input.indexOf("an",index)!== -1){
+        index = input.indexOf("an", index)+1;
+        aantalKeerAn++;
+    }
+
+    aantal.textContent = " = " + aantalKeerAn + " keer";
 }
 
 window.addEventListener("load", setup);

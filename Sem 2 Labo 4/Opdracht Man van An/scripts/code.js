6cc2d950397b7b49c4223bc78626e2b10686fab3
SimonOmey <simon.omey@student.vives.be>
2024-03-09 18:36:28 +0100

    lastIndexof fixed

--- Sem 2 Labo 4/Opdracht Man van An/scripts/code.js
+++ Sem 2 Labo 4/Opdracht Man van An/scripts/code.js
@@ -1,6 +1,8 @@
 const setup = () => {
 	let button = document.getElementById("button")
     button.addEventListener('click',berekenAn)
+    let button2 = document.getElementById("button2")
+    button2.addEventListener('click',berekenAn2)
 }
 
 const berekenAn = () =>{
@@ -16,6 +18,21 @@ const berekenAn = () =>{
     }
 
     aantal.textContent = " = " + aantalKeerAn + " keer";
+}
+
+const berekenAn2 = () =>{
+    let input2 = document.getElementById("input2").value
+    input2 = input2.toLowerCase();
+    let aantal2 = document.getElementById("aantal2")
+    let index = input2.length
+    let aantalKeerAn2 = 0
+
+    while(input2.lastIndexOf("an",index)!== -1){
+        index = input2.lastIndexOf("an", index)-1;
+        aantalKeerAn2++;
+    }
+
+    aantal2.textContent = " = " + aantalKeerAn2 + " keer";
 }
 
 window.addEventListener("load", setup);
